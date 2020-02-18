416985b3d7dd9c503f8a30d67f8b780eaf6d6c4b
root <root@web01-aaron.aaron.local>
2020-02-18 13:56:58 -0500

    secure shh script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author aaronleach26
+#creates a new ssh user using $l parameter
+#adds a public key from the lcoal repo or curled from the remote repo
+#removes root ability to shh in
+echo "ALL YOUR CODE GOES HERE"
