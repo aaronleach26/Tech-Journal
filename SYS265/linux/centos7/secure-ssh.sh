7f0b70f838130de23d3e9831211a2eb6710747ff
aaronleach26 <aaron.leach@mymail.champlain.edu>
2020-02-24 20:42:48 +0000

    add sercure-ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,10 @@
 #creates a new ssh user using $l parameter
 #adds a public key from the lcoal repo or curled from the remote repo
 #removes root ability to shh in
-echo "ALL YOUR CODE GOES HERE"
+
+useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp /home/aaron/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
